22aedfeff2e4ef07fe7930194e5bd548f5fe2305
asmalouski <asmalaiks@gmail.com>
2015-05-31 20:47:46 +0300

    first one

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+
+
+public class Main {
+
+    public static void main(String args[]){
+
+    }
+
+
+}
---
+++ src/Reader.java
@@ -0,0 +1,12 @@
+import javax.swing.*;
+
+/**
+ * Created by asmalouski on 31.5.15.
+ */
+public class Reader {
+
+    public void scan(){
+
+    }
+
+}
